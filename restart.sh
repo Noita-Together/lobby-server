a27108dd3b19dc5dbc5efaece155e35aae4e8c2f
Kris Reeves <kris@pressbuttonllc.com>
2023-11-27 00:23:18 +0000

    Update restart script for new wiring

--- restart.sh
+++ restart.sh
@@ -8,25 +8,26 @@ source "$HERE/.env"
 
 CONTAINER_NAME="lobby-server"
 IMAGE_NAME="lobby-server"
-TLS_SERVER_NAME="dev.noitatogether.com"
+TLS_SERVER_NAME="lobby.noitatogether.com"
+ANCHOR_IP="$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/anchor_ipv4/address)"
 
 docker stop "$CONTAINER_NAME" || true
 docker rm "$CONTAINER_NAME" || true
 
 docker run -d --name "$CONTAINER_NAME" \
+  --restart "unless-stopped" \
   --network "nt" \
   --network-alias "$TLS_SERVER_NAME" \
-  --restart "unless-stopped" \
   -v "/etc/letsencrypt/archive/$TLS_SERVER_NAME:/etc/letsencrypt/archive/$TLS_SERVER_NAME" \
   -v "/etc/letsencrypt/live/$TLS_SERVER_NAME:/etc/letsencrypt/live/$TLS_SERVER_NAME" \
-  -p "$LOBBY_WEBSOCKET_PORT:$LOBBY_WEBSOCKET_PORT" \
+  -p "$ANCHOR_IP:443:443" \
   -e "DEBUG=*" \
   -e "JWT_SECRET=$SECRET_JWT_ACCESS" \
   -e "JWT_REFRESH=$SECRET_JWT_REFRESH" \
   -e "TLS_SERVER_NAME=$TLS_SERVER_NAME" \
   -e "TLS_CERT_FILE=/etc/letsencrypt/live/$TLS_SERVER_NAME/fullchain.pem" \
   -e "TLS_KEY_FILE=/etc/letsencrypt/live/$TLS_SERVER_NAME/privkey.pem" \
-  -e "WS_PORT=$LOBBY_WEBSOCKET_PORT" \
-  -e "STATS_URL_TEMPLATE=https://dev.noitatogether.com/api/stats/[ROOM_ID]/[STATS_ID]/html" \
-  -e "WEBFACE_ORIGIN=https://dev.noitatogether.com" \
+  -e "APP_LISTEN_PORT=443" \
+  -e "STATS_URL_TEMPLATE=https://noitatogether.com/api/stats/[ROOM_ID]/[STATS_ID]/html" \
+  -e "WEBFACE_ORIGIN=https://noitatogether.com" \
   "$IMAGE_NAME"
